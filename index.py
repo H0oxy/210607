8e76e3ff78492a73cbaf4796b19cb4289832fd4b
H0oxy <xMolod0yx.Arseny@yandex.ru>
2021-06-07 11:18:00 +0500

    class

---
+++ index.py
@@ -0,0 +1,30 @@
+class Student(object):
+    """docstring"""
+
+    def __init__(self):
+        """Constructor"""
+        pass
+
+
+class Name(object):
+    """docstring"""
+
+    def __init__(self):
+        """Constructor"""
+        pass
+
+
+class DoB(object):
+    """docstring"""
+
+    def __init__(self):
+        """Constructor"""
+        pass
+
+
+class Address(object):
+    """docstring"""
+
+    def __init__(self):
+        """Constructor"""
+        pass
