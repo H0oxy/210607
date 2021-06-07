45d33b3470b01bbd4269725fdaa39199e59ede44
H0oxy <xMolod0yx.Arseny@yandex.ru>
2021-06-07 11:35:16 +0500

    class Group

--- index.py
+++ index.py
@@ -1,8 +1,17 @@
-class Student(object):
+class Student:
 
-    def __init__(self, name, dateofbirth, address):
+    def __init__(self, name, birth_date, address):
         self.name = name
-        self.dateofbirth = dateofbirth
+        self.dateofbirth = birth_date
         self.address = address
+        pass
+
+
+
+class Group:
+
+    def __init__(self, name):
+        self.name = name
+        self.student = Student
         pass
 
