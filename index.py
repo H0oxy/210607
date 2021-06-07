836ab17e18f6093db230e49f4e3b568158d937e0
H0oxy <xMolod0yx.Arseny@yandex.ru>
2021-06-07 11:25:19 +0500

    class

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 class Student(object):
 
-    def __init__(self):
+    def __init__(self, name, dateofbirth, address):
         self.name = name
         self.dateofbirth = dateofbirth
         self.address = address
